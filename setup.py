ee2a54d27b5f196e858b4895ddafa5ca477aa4a7
jaimebw <jaimebwv@gmail.com>
2021-11-29 13:08:47 +0100

    Added change to v1.0, and finished!!

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 setup(
         name = 'jupyter_cell_extractor',
-        version='0.1.0'
+        version='1.0.0'
         description= "Jupyter Cell Extractor",
-        long_description= long_description
+        long_description= long_description,
+        long_description_content_type='text/markdown',
         package_dir={"": "src"},
         packages=setuptools.find_packages(where="src"),
         python_requires=">=3.8"
